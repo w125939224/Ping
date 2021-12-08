39508ef9d5757cb5efe80efced3bb3f33e5187ec
钦奕 王 <w125939224@qq.com>
2021-12-08 16:31:02 +0800

    添加项目文件。

---
+++ Ping_main.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace PingResult
+{
+    partial class Ping_main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Begin = new System.Windows.Forms.Button();
+            this.Import = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Pinglist = new System.Windows.Forms.RichTextBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.Datashow = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Datashow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Begin
+            // 
+            this.Begin.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.Begin.AutoSize = true;
+            this.Begin.Location = new System.Drawing.Point(3, 3);
+            this.Begin.Name = "Begin";
+            this.Begin.Size = new System.Drawing.Size(87, 34);
+            this.Begin.TabIndex = 1;
+            this.Begin.Text = "开始ping测试";
+            this.Begin.UseVisualStyleBackColor = true;
+            this.Begin.Click += new System.EventHandler(this.begin_Click);
+            // 
+            // Import
+            // 
+            this.Import.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.Import.AutoSize = true;
+            this.Import.Location = new System.Drawing.Point(240, 3);
+            this.Import.Name = "Import";
+            this.Import.Size = new System.Drawing.Size(76, 34);
+            this.Import.TabIndex = 2;
+            this.Import.Text = "导出结果";
+            this.Import.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.Begin);
+            this.splitContainer1.Panel1.Controls.Add(this.Import);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.Pinglist);
+            this.splitContainer1.Size = new System.Drawing.Size(319, 631);
+            this.splitContainer1.SplitterDistance = 40;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // Pinglist
+            // 
+            this.Pinglist.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Pinglist.Location = new System.Drawing.Point(0, 0);
+            this.Pinglist.Name = "Pinglist";
+            this.Pinglist.Size = new System.Drawing.Size(319, 587);
+            this.Pinglist.TabIndex = 0;
+            this.Pinglist.Text = "";
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.Datashow);
+            this.splitContainer2.Size = new System.Drawing.Size(975, 637);
+            this.splitContainer2.SplitterDistance = 325;
+            this.splitContainer2.TabIndex = 4;
+            // 
+            // Datashow
+            // 
+            this.Datashow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Datashow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Datashow.Location = new System.Drawing.Point(0, 0);
+            this.Datashow.Name = "Datashow";
+            this.Datashow.RowTemplate.Height = 23;
+            this.Datashow.Size = new System.Drawing.Size(646, 637);
+            this.Datashow.TabIndex = 0;
+            // 
+            // Ping_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 637);
+            this.Controls.Add(this.splitContainer2);
+            this.Name = "Ping_main";
+            this.Text = "Ping_main";
+            this.Load += new System.EventHandler(this.Ping_main_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Datashow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Begin;
+        private System.Windows.Forms.Button Import;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.DataGridView Datashow;
+        private System.Windows.Forms.RichTextBox Pinglist;
+    }
+}
+
---
+++ Ping_main.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PingResult
+{
+    public partial class Ping_main : Form
+    {
+        public Ping_main()
+        {
+            InitializeComponent();
+        }
+        private void begin_Click(object sender, EventArgs e)
+        {
+            var PingLinesCount = Pinglist.Lines.Count();
+            try
+            {
+
+                for (int i = 0; i < PingLinesCount; i++)
+                {
+                    if (Pinglist.Text != null)
+                    {
+                        var NetSefment = Pinglist.Lines[i];
+                        string[] NetSefmentArray = NetSefment.Split('-', ',', ' ', '。', '~',':');
+                        var BeginNetSegment = NetSefmentArray[0];
+                        var EndNetSegment = NetSefmentArray[1];
+                        if (IPAddress.TryParse(BeginNetSegment, out var BeginNetSegmentIP)
+                            && IPAddress.TryParse(EndNetSegment, out var EndNetSegmentIP)
+                            && BitConverter.ToInt32(BeginNetSegmentIP.GetAddressBytes(),0)
+                            >= BitConverter.ToInt32(EndNetSegmentIP.GetAddressBytes(),0))
+                        {
+                            
+                            //Dns.GetHostName
+                        }
+                        else
+                        {
+                            MessageBox.Show("无效IP地址，请检查后重新输入。");
+                        }
+                    }
+                    else break;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("程序发生未知错误。");
+            }
+        }
+        private void Ping_main_Load(object sender, EventArgs e)
+        {
+            Pinglist.Text = "请在此输入需要扫描的IP段，格式为一行一个IP段。例：192.168.0.1-192.168.0.254。";
+            Pinglist.HideSelection = false;
+            Pinglist.SelectAll();
+        }
+    }
+}
